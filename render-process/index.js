22252b00c1adde55c2262e35748c92e93d4aa119
Vineet Sing <vinsin@umd.edu>
2023-03-30 16:59:52 -0400

    path correction

--- render-process/index.js
+++ render-process/index.js
@@ -1,8 +1,5 @@
 const { BrowserWindow } = require('@electron/remote')
 
-const path = require("path");
-const url = require("url");
-
 const newWindowBtn = document.getElementById('newWindowBt');
 
 newWindowBtn.addEventListener('click', function(){
@@ -11,14 +8,7 @@ newWindowBtn.addEventListener('click', function(){
         title: 'test 2',
         width:200,
         height: 300,
-        // webPreferences: {
-        //     nodeIntegration: true,
-        //     contextIsolation: false,
-        //     enableRemoteModule: true,
-        
-        // }
     });
-    win.webContents.openDevTools();
-    // win.loadFile(path.join(path.join(__dirname, 'index.html')));
-    win.loadFile('./home.html');
+    // win.webContents.openDevTools();
+    win.loadFile('./render-process/home.html');
 });
