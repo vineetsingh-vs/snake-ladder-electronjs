4225e48f37530e0c83b3ea1484569491de1bb5d1
Vineet Sing <vinsin@umd.edu>
2023-03-30 16:39:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { BrowserWindow } = require('@electron/remote')
+
+const path = require("path");
+const url = require("url");
+
+const newWindowBtn = document.getElementById('newWindowBt');
+
+newWindowBtn.addEventListener('click', function(){
+    debugger;
+    let win= new BrowserWindow({
+        title: 'test 2',
+        width:200,
+        height: 300,
+        // webPreferences: {
+        //     nodeIntegration: true,
+        //     contextIsolation: false,
+        //     enableRemoteModule: true,
+        
+        // }
+    });
+    win.webContents.openDevTools();
+    // win.loadFile(path.join(path.join(__dirname, 'index.html')));
+    win.loadFile('home.html');
+});
---
+++ main.js
@@ -0,0 +1,51 @@
+console.log('main process working');
+
+const {BrowserWindow, app} = require('electron');
+const path = require("path");
+require('@electron/remote/main').initialize()
+
+let win;
+const isDev = process.env.ENV_NODE !== 'production';
+const isMac = process.platform !== 'darwin';
+
+const createWindow = ()=>{
+    win= new BrowserWindow({
+        title: 'test 1',
+        width: isDev ? 1000: 500,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    if(isDev) {
+        // win.webContents.openDevTools();
+    }
+    win.loadFile('index.html');
+    
+
+    win.on('closed', () =>{
+        win = null;
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () =>{
+        if(win === null) {
+           createWindow();
+    }});
+});
+
+app.on('window-all-closed', () =>{
+    if(isMac) {
+        app.quit();
+    }
+
+});
+
+app.on('browser-window-created', (_, window) => {
+    require("@electron/remote/main").enable(window.webContents)
+})
